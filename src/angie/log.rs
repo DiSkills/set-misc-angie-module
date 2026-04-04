4916ee6e43fbca155bba4c168264ea94206ca4d3
DiSkills <github@diskills-developer.org>
2026-04-04 18:29:50 +0300

    Added log::Level from ngx::wasi to log.rs

--- src/angie/log.rs
+++ src/angie/log.rs
@@ -5,6 +5,13 @@
 
 include!("../../bind/ngx_wasi_core.rs");
 
+wit_bindgen::generate!({
+    world: "angie-set-misc",
+    generate_all,
+});
+
+pub use ngx::wasi::log::Level;
+
 pub fn log_error(fd: i32, level: u8, errnum: i32, msg: String) {
     let mut message = ngx_wasi_core_string_t {
         ptr: msg.as_ptr() as *mut u8, len: msg.len(),

2a65e4eb8ad95e63cb14d7ea2dbc6687ab4b8038
DiSkills <github@diskills-developer.org>
2026-04-16 08:55:34 +0300

    Moved generation to lib.rs

--- src/base64.rs
+++ src/base64.rs
@@ -1,17 +1,8 @@
-wit_bindgen::generate!({
-    world: "angie-set-misc",
-    generate_all,
-});
+use base64::{engine::general_purpose::STANDARD, Engine};
 
-use base64::engine::general_purpose::STANDARD;
-use base64::Engine;
+use crate::{common, exports::angie::set_misc, Entry};
 
-use crate::common;
-
-struct Entry;
-export!(Entry);
-
-impl exports::angie::set_misc::base64::Guest for Entry {
+impl set_misc::base64::Guest for Entry {
     fn encode(call_env: i32) -> i32 {
         common::call_single_argument_variable_handler(call_env, encode)
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+wit_bindgen::generate!({
+    world: "angie-set-misc",
+    generate_all,
+});
+
 mod angie;
 mod base64;
 mod common;
+
+struct Entry;
+export!(Entry);

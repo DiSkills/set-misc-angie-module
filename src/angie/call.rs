49411931c81504ade44cac13ba449bb02d811cc1
DiSkills <github@diskills-developer.org>
2026-05-01 12:22:14 +0300

    Refactored SDK adapter to remove the dependency between common.rs and Angie WASM SDK

--- src/angie/call.rs
+++ src/angie/call.rs
@@ -3,30 +3,48 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 
+use crate::angie::app::State;
+
 include!("../../bind/ngx_wasi_core.rs");
 
-pub fn init_app(call_env: i32, argv: bool) -> Result<ngx_wasi_app_state_t, i32> {
+pub fn init_app(call_env: i32, argv: bool) -> Result<State, i32> {
     let mut app = ngx_wasi_app_state_t {
         internal: 0, ctx: 0, log: 0,
         argv: ngx_wasi_app_str_array_t { ptr: std::ptr::null_mut(), len: 0, },
     };
     let mut err: i32 = -1;
 
-    let res: bool = unsafe { exports_ngx_wasi_call_init_app(call_env, argv, &mut app, &mut err) };
-    if res {
-        Ok(app)
-    } else {
-        Err(err)
+    let res: bool = unsafe {
+        exports_ngx_wasi_call_init_app(call_env, argv, &mut app, &mut err)
+    };
+    if res != true {
+        return Err(err);
     }
+
+    let mut args = Vec::new();
+    let slice = unsafe {
+        std::slice::from_raw_parts(app.argv.ptr, app.argv.len)
+    };
+    for el in slice.iter() {
+        let item = *el;
+
+        let arg = unsafe {
+            match std::str::from_utf8(std::slice::from_raw_parts(item.ptr, item.len)) {
+                Ok(v) => v, Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+            }
+        };
+        args.push(arg.to_string());
+    }
+    Ok(State { internal: app.internal, ctx: app.ctx, log: app.log, argv: args })
 }
 
-pub fn save_result(app: ngx_wasi_app_state_t, buf: Vec<u8>) -> i32 {
+pub fn save_result(app: State, buf: Vec<u8>) -> i32 {
     let mut b = exports_ngx_wasi_call_res_t {
         ptr: buf.as_ptr() as *mut u8, len: buf.len(),
     };
-    let app_pointer = &app as *const _;
-
-    unsafe {
-        exports_ngx_wasi_call_save_result(app_pointer as *mut _, &mut b)
-    }
+    let mut app = ngx_wasi_app_state_t {
+        internal: app.internal, ctx: app.ctx, log: app.log,
+        argv: ngx_wasi_app_str_array_t { ptr: std::ptr::null_mut(), len: 0, },
+    };
+    unsafe { exports_ngx_wasi_call_save_result(&mut app, &mut b) }
 }
--- src/common.rs
+++ src/common.rs
@@ -1,10 +1,3 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(dead_code)]
-
-include!("../bind/ngx_wasi_core.rs");
-
 use crate::angie::{call, log};
 
 pub struct HandlerError {
@@ -18,20 +11,15 @@ pub fn call_single_argument_variable_handler(
     let app = match call::init_app(call_env, true) {
         Ok(v) => v, Err(err) => return err,
     };
-    if app.argv.len != 1 {
+
+    if app.argv.len() != 1 {
         log::log_error(
             app.log, log::Level::Err as u8, 0,
             String::from("error: exactly one argument is expected"),
         );
         return -1;
     }
-
-    let raw = unsafe { *app.argv.ptr };
-    let argument = unsafe {
-        match std::str::from_utf8(std::slice::from_raw_parts(raw.ptr, raw.len)) {
-            Ok(v) => v, Err(err) => panic!("Invalid UTF-8 sequence: {}", err),
-        }
-    }.to_string();
+    let argument = app.argv.get(0).unwrap().to_string();
 
     let result = match handler(argument) {
         Ok(v) => v,
